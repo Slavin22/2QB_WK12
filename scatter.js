1549b421d81f7ac12e7065168015c34284a8f05b
Sean Slavin <sean@Seans-MBP.home>
2017-11-22 21:04:10 -0500

    Initialized files

---
+++ scatter.js
@@ -0,0 +1,33 @@
+var trace1 = {
+	x: qbData.map(row => row.adjPA),
+	y: qbData.map(row => row.adjPF),
+	mode: 'markers',
+	type: 'scatter',
+	text: matchups,
+	marker: {size: 20,
+			color: qbData.map(row => row.Proj),
+			colorscale: [[0, 'rgb(205,0,0)'], [0.5, 'rgb(255,235,0)'],[1, 'rgb(0,128,0)']]
+			},
+	hoverinfo: 'text',
+	hoverlabel: {bgcolor: 'black', bordercolor: 'white'},
+	fillcolor: "black"
+}
+
+var data = [trace1];
+
+var layout = {
+  xaxis: {
+    range: [9.5, 23.5],
+    title: 'Defense - Adjusted QB Points Allowed'
+  },
+  yaxis: {
+    range: [7.5, 23.5],
+    title: 'QB - Adjusted Points Scored'
+  },
+  title:'Week 12 QB Projections (hover over bubbles to find the QB + matchup info)',
+  hovermode: 'closest'
+};
+
+Plotly.newPlot("plot", data, layout)
+
+renderTable()
